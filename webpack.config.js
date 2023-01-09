d30940d79ec1ca155ac29a224595a17fb716f252
surya <ssuryakumar97@gmail.com>
2023-01-09 15:15:21 +0530

    webpack changed

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 module.exports = {
     resolve :{
-        fallback: { buffer: require.resolve("buffer/") },
-        fallback: { os: require.resolve("os-browserify/browser") },
-        fallback: { path: require.resolve("path-browserify") }
+        fallback: { buffer: require.resolve("buffer/") }
     }
 }
 
